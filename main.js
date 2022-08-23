518082faa7063f34677092a9f7150050e692b1ab
Erick Fonseca <erick.peixoto@assert.ifpb.edu.br>
2022-08-22 23:31:50 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+let operacao = "/";
+let num1 = 10, num2 = 5;
+
+switch (operacao) {
+  case "+": {
+    console.log(num1 + num2);
+    break;
+  }
+  case "-": {
+    console.log(num1 - num2);
+    break;
+  }
+  case "*": {
+    console.log(num1 * num2);
+    break
+  }
+  case "/": {
+    console.log(num1 / num2);
+    break
+  }
+  default: {
+    console.log("Não é uma operação válida");
+    break;
+  }
+}
